5168aca5b7cb43f2254458bd1ae373804665c59d
Akame-moe <819548948@qq.com>
2020-05-14 22:01:12 +0800

    format code.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //for fetch data from firewall outside purpose
 
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+	event.respondWith(handleRequest(event.request))
 })
 
 /**
@@ -10,38 +10,31 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-	
-	try{
-		
-		
-	  var u = new URL(request.url);
-	  var h = new Headers(request.headers);
-	  var q = u.searchParams.get('q');
-	  if(q){
-			  const opt = {
+	try {
+		var u = new URL(request.url);
+		var h = new Headers(request.headers);
+		var q = u.searchParams.get('q');
+		if (q) {
+			const opt = {
 				method: request.method,
 				headers: h,
 				redirect: 'follow',
 				body: request.body
 			};
-		  var req_url = decodeURIComponent(q);
-		  var resp = await fetch(req_url,opt);
-			//return new Response(resp.body, {status: 500});
-		  var resp_headers = new Headers(resp.headers);
-		  resp_headers.set('access-control-allow-origin', '*');
-		  resp_headers.set('access-control-expose-headers', '*');
-		  resp_headers.set('Author', 'Akame');
-		  resp_headers.delete('content-security-policy');
-		  resp_headers.delete('content-security-policy-report-only');
-		  resp_headers.delete('clear-site-data');
-		  var len = resp_headers.get('content-length');
-		  return new Response(resp.body, {status: 200,headers:resp_headers});
-	  }else{
-		  return new Response('invalid parameters.', {status: 400});
-	  }
+			var req_url = decodeURIComponent(q);
+			var resp = await fetch(req_url, opt);
+			var resp_headers = new Headers(resp.headers);
+			resp_headers.set('access-control-allow-origin', '*');
+			resp_headers.set('access-control-expose-headers', '*');
+			resp_headers.set('Author', 'Akame');
+			var len = resp_headers.get('content-length');
+			return new Response(resp.body, { status: 200, headers: resp_headers });
+		} else {
+			return new Response('invalid parameters.', { status: 400 });
+		}
 
-  }catch(e){
-	  return new Response('ERROR->'+e, {status: 500});
-  }
-  
+	} catch (e) {
+		return new Response('ERROR->' + e, { status: 500 });
+	}
+
 }
